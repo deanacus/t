712f636b7abcc61d08c457f6e3856d1916c264a0
Dean Harris <dean.harris@comparethemarket.com.au>
2019-10-28 19:25:25 +1000

    feat(files): add ability to read/write files

--- readfile.go
+++ readfile.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	// "bufio"
 	"fmt"
-	// "io"
 	"io/ioutil"
 	"os"
 )
 
-
-
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
 
 func check(e error) {
 	if e != nil {
-			panic(e)
+		panic(e)
 	}
 }
 
-
 func readfile() {
 	HOME, err := os.UserHomeDir()
-	dat, err := ioutil.ReadFile(HOME + "/.todo")
-	fmt.Println(HOME + "/.todo");
 	check(err)
-	fmt.Print(string(dat))
+	if fileExists(HOME + "/.todo") {
+		dat, err := ioutil.ReadFile(HOME + "/.todo")
+		fmt.Println(HOME + "/.todo")
+		check(err)
+		fmt.Print(string(dat))
+	} else {
+		dat, err := os.Create("./.todo")
+		check(err)
+		fmt.Println("File did not exist. It has been created")
+		fmt.Println(dat)
+	}
 }
